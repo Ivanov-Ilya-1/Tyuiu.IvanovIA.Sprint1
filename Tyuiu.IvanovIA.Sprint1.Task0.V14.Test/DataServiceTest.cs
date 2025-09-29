3f8de2584165de28fdf0562cc238e7aac7d9cfff
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-29 14:44:30 +0300

    Add project files.

---
+++ Tyuiu.IvanovIA.Sprint1.Task0.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.IvanovIA.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.IvanovIA.Sprint1.Task0.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(25, res);
+
+        }
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint1.Task0.V14/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.IvanovIA.Sprint1.Task0.V14.Lib;
+namespace Tyuiu.IvanovIA.Sprintq.Task0.V14
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #1 | Выполнил: Иванов И. А. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Спринт #1                                                                *");
+            Console.WriteLine(" Тема : Базовые навыки работы в C#                                        *");
+            Console.WriteLine(" Задание #0                                                               *");
+            Console.WriteLine(" Вариант #14                                                              *");
+            Console.WriteLine(" Выполнил : Иванов Илья Анатольевич | ИИПБ-25-1                           *");
+            Console.WriteLine(" Спринт #1                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" УСЛОВИЕ:                                                                 *");
+            Console.WriteLine(" Написать программу, которая вычисляет выражение 2*3*3+7                  *");
+            Console.WriteLine(" и печатает результат на экране.                                          *");
+            Console.WriteLine(" *                                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+            Console.ReadKey();
+
+        }
+    }
+}
