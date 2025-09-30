b2639c8bdfa5e96d7e97557a93c86eefbe9edc7e
tyuiu.ivanovia@outlook.com <gilya0407@gmail.com>
2025-09-30 16:34:17 +0300

    Task2

---
+++ Tyuiu.IvanovIA.Sprint1.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.IvanovIA.Sprint1.Task2.V14.Lib;
+namespace Tyuiu.IvanovIA.Sprint1.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExperession()
+        {
+            DataService ds = new DataService(); 
+            int x = 274;
+            var res = ds.ConvertKelvinToCelsius(x);
+            Assert.AreEqual(1, res);
+        }
+
+
+    }
+}
---
+++ Tyuiu.IvanovIA.Sprint1.Task2.V14/Program.cs
@@ -0,0 +1,45 @@
+﻿using Tyuiu.IvanovIA.Sprint1.Task2.V14.Lib;
+namespace Tyuiu.IvanovIA.Sprint1.Task2.V14
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #1 | Выполнил: Иванов И. А. | ИИПБ-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" Спринт #1                                                                *");
+            Console.WriteLine(" Тема : Арифметические операторы в C#                                     *");
+            Console.WriteLine(" Задание #2                                                               *");
+            Console.WriteLine(" Вариант #14                                                              *");
+            Console.WriteLine(" Выполнил : Иванов Илья Анатольевич | ИИПБ-25-1                           *");
+            Console.WriteLine(" Спринт #1                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" УСЛОВИЕ:                                                                 *");
+            Console.WriteLine(" Известна температура в градусах Кельвина. Перевести температуру          *");
+            Console.WriteLine(" в градусы Цельсия.                                                       *");
+            Console.WriteLine(" *                                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите температуру в градусах Кельвина: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(" РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Температура в градусах Цельсия: {ds.ConvertKelvinToCelsius(x)}");
+            Console.ReadKey();
+
+        }
+    }
+}
